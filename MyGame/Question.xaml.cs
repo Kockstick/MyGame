c243d69e2540fe4e0ccc66ba54f282cab683f5c9
Kockstick <kostyadi04@mail.ru>
2023-03-30 17:36:06 +0700

    row

---
+++ MyGame/Question.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace MyGame
+{
+    public partial class Question : Border
+    {
+        private int _score;
+        public int score
+        {
+            get
+            {
+                return _score;
+            }
+            set
+            {
+                scoreLabel.Content = value.ToString();
+                _score = value;
+            }
+        }
+        public string text { get; set; }
+        public string answer { get; set; }
+        public bool isShowed { get; set; }
+
+        public Question()
+        {
+            InitializeComponent();
+        }
+
+        public Question(int score, string text, string answer, bool isShowed) : this()
+        {
+            this.score = score;
+            this.text = text;
+            this.answer = answer;
+            this.isShowed = isShowed;
+        }
+    }
+}
